7e9947ede9b6a9aa111bb85886f2988281afe820
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-20 22:49:57 +0200

    9.2-poch

---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const refs = {
+    input: document.querySelector('#datetime-picker'),
+    btn: document.querySelector('[data-start]'),
+    daysEl: document.querySelector('[data-days]'),
+    hoursEl: document.querySelector('[data-hours]'),
+    minutesEl: document.querySelector('[data-minutes]'),
+    secondsEl: document.querySelector('[data-seconds]'),
+    body: document.querySelector('body'),
+  };
+  
+
+  
