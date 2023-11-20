8834f4d9e7de12ca0264d84ca18ad8a66fa817bf
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-20 20:46:02 +0200

    do-click

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
-const proverka = ye
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  const refs = {
+    start: document.querySelector('batton[data-start]');
+    stop: document.querySelector('batton[data-stop]');
+    body: document.querySelector('body');
+  }
+
+  refs.start.addEventListener('click', clickStart);
+refs.stop.addEventListener('click', clickStop);
+const DELEY = 1000;
+let id = null;
+refs.stop.setAttribute('disabled', 'true');
