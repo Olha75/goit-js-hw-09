b0d27d88ee40d809a69a30128eafca2aa0097e7c
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-20 23:03:00 +0200

    9.2-timer-ded

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,54 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-const refs = {
-    input: document.querySelector('#datetime-picker'),
-    btn: document.querySelector('[data-start]'),
-    daysEl: document.querySelector('[data-days]'),
-    hoursEl: document.querySelector('[data-hours]'),
-    minutesEl: document.querySelector('[data-minutes]'),
-    secondsEl: document.querySelector('[data-seconds]'),
-    body: document.querySelector('body'),
+  const timerElements = {
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
   };
+  const datetimePicker = flatpickr("#datetime-picker", {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      const selectedDate = selectedDates[0];
+      const currentDate = new Date();
   
+      if (selectedDate <= currentDate) {
+        Notiflix.Notify.warning("Будь ласка, оберіть дату у майбутньому");
+        document.querySelector('[data-start]').disabled = true;
+      } else {
+        document.querySelector('[data-start]').removeAttribute('disabled');
+      }
+    },
+  });
+  
+let countdownInterval;
 
+function clickStart() {
+  const selectedDate = datetimePicker.selectedDates[0];
+  const currentDate = new Date();
+  const timeDifference = selectedDate - currentDate;
+
+  countdownInterval = setInterval(() => {
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+    timerElements.days.textContent = addLeadingZero(days);
+    timerElements.hours.textContent = addLeadingZero(hours);
+    timerElements.minutes.textContent = addLeadingZero(minutes);
+    timerElements.seconds.textContent = addLeadingZero(seconds);
+
+    if (timeDifference <= 0) {
+      clearInterval(countdownInterval);
+      Notiflix.Notify.success("Відлік завершено!");
+      document.querySelector('[data-start]').disabled = true;
+    }
+
+    timeDifference -= 1000;
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart}
   
