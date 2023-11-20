d4783d634ebf517c33072e59d5dc342d8844ab1f
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-20 20:48:59 +0200

    comm

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,14 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-  const refs = {
-    start: document.querySelector('batton[data-start]');
-    stop: document.querySelector('batton[data-stop]');
-    body: document.querySelector('body');
-  }
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+//   }
+//   const refs = {
+//     start: document.querySelector('batton[data-start]');
+//     stop: document.querySelector('batton[data-stop]');
+//     body: document.querySelector('body');
+//   }
 
-  refs.start.addEventListener('click', clickStart);
-refs.stop.addEventListener('click', clickStop);
-const DELEY = 1000;
-let id = null;
-refs.stop.setAttribute('disabled', 'true');
+//   refs.start.addEventListener('click', clickStart);
+// refs.stop.addEventListener('click', clickStop);
+// const DELEY = 1000;
+// let id = null;
+// refs.stop.setAttribute('disabled', 'true');
