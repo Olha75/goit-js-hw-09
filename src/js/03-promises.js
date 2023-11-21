a54ab06796f760ccc074b6db3a91837dd522309c
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-21 22:41:36 +0200

    3.33

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,10 +6,42 @@ const { delay, step, amount } = form.elements;
 form.addEventListener('submit', onSubmit);
 
 function createPromise(position, delay) {
+  return new Promise((resolve, reject)=>{
   const shouldResolve = Math.random() > 0.3;
+setTimeout(()=>{
   if (shouldResolve) {
     // Fulfill
+    resolve({position, delay})
   } else {
     // Reject
+    reject({position,delay})
+  }
+}, delay);
+});
+}
+
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  let amountValue = Number(amount.value);
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    let generalDelay = delayValue + stepValue * i - stepValue;
+
+    createPromise(i, generalDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
+
